f77d552e796a6b89e1d292be681b3d376f753fdf
bacongubbe <94456001+bacongubbe@users.noreply.github.com>
2023-09-07 13:54:11 +0200

    initail commit

---
+++ src/test/java/dev/bacongubbe/wishlist/WishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bacongubbe.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
