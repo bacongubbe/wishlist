46857320aba60471cf37cc327e7ba581b7e72f73
bacongubbe <94456001+bacongubbe@users.noreply.github.com>
2023-10-06 16:56:01 +0200

    Feat: Add login option to Swagger

--- src/main/java/dev/bacongubbe/wishlistapp/WishlistApplication.java
+++ src/main/java/dev/bacongubbe/wishlistapp/WishlistApplication.java
@@ -1,8 +1,16 @@
 package dev.bacongubbe.wishlistapp;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SecurityScheme(
+    name = "Bearer Authentication",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
 @SpringBootApplication
 public class WishlistApplication {
 
