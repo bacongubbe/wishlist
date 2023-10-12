463e8527369778281f10ca3e49a39b0622b1c264
bacongubbe <94456001+bacongubbe@users.noreply.github.com>
2023-10-12 10:24:41 +0200

    Feat: Add validation to avoid SQL injection

--- src/main/java/dev/bacongubbe/wishlistapp/wishlist/CreateWishlistDto.java
+++ src/main/java/dev/bacongubbe/wishlistapp/wishlist/CreateWishlistDto.java
@@ -2,10 +2,12 @@ package dev.bacongubbe.wishlistapp.wishlist;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record CreateWishlistDto(
     @NotBlank
     @NotNull
+    @Pattern(regexp = "[^;{}\"^\\\\]*")
     String name
 ) {
 }
