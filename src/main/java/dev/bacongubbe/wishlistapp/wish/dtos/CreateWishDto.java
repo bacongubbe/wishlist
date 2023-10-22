f1905de01fd3b7c8c96d241739b2ab39904215dd
bacongubbe <94456001+bacongubbe@users.noreply.github.com>
2023-10-22 12:50:26 +0200

    Add pattern matcher to CreateWishDto

--- src/main/java/dev/bacongubbe/wishlistapp/wish/dtos/CreateWishDto.java
+++ src/main/java/dev/bacongubbe/wishlistapp/wish/dtos/CreateWishDto.java
@@ -2,9 +2,11 @@ package dev.bacongubbe.wishlistapp.wish.dtos;
 
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record CreateWishDto(
     @NotBlank(message = "Wish cannot be blank")
+    @Pattern(regexp = "[^;{}\"^\\\\]*", message = "No hacking allowed.")
     String wish,
     @Nullable
     String link,
